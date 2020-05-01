c73c2475699ae310d128773bd151a839c96758b0
dhruvparihar11 <64633002+dhruvparihar11@users.noreply.github.com>
2020-05-01 13:41:22 +0530

    Removed namespace directive

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
-using namespace std:
+
 
 int main()
 {
-    cout << "Hello GitHub!";
+    std::cout << "Hello GitHub!" << std::endl;
+    std::cin.get();
     
     result 0;
     
